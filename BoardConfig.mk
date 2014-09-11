e623ae7090fac2bbc476fba16dd19fbefe33be93
Artur <baran0119@users.noreply.github.com>
2014-09-11 10:11:58 +0200

    Update BoardConfig.mk
    check wifi

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -67,25 +67,26 @@ TARGET_PROVIDES_CAMERA_HAL := true
 USE_DEVICE_SPECIFIC_CAMERA := true
 
 # Connectivity - Wi-Fi
-WPA_BUILD_SUPPLICANT            := true
-WPA_SUPPLICANT_VERSION          := VER_0_8_X
-BOARD_WLAN_DEVICE               := bcmdhd
-BOARD_WLAN_DEVICE_REV 	        := bcm4330_b1
-BOARD_WPA_SUPPLICANT_DRIVER     := NL80211
+WPA_BUILD_SUPPLICANT := true
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_WLAN_DEVICE := bcmdhd
+# BOARD_WLAN_DEVICE_REV := bcm4330
+BOARD_WLAN_DEVICE_REV := bcm4330_b1
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-BOARD_HOSTAPD_DRIVER            := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB       := lib_driver_cmd_bcmdhd
-WIFI_DRIVER_MODULE_PATH         := "/system/lib/modules/dhd.ko"
-WIFI_DRIVER_FW_PATH_PARAM       := "/sys/module/dhd/parameters/firmware_path"
-WIFI_DRIVER_FW_PATH_STA         := "/system/etc/wifi/bcm4330_sta.bin"
-WIFI_DRIVER_FW_PATH_AP          := "/system/etc/wifi/bcm4330_apsta.bin"
-WIFI_DRIVER_FW_PATH_P2P         := "/system/etc/wifi/bcm4330_p2p.bin"
-WIFI_DRIVER_MODULE_NAME         := "dhd"
-WIFI_DRIVER_MODULE_ARG      	:= "firmware_path=/system/etc/wifi/bcm4330_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
-WIFI_DRIVER_MODULE_AP_ARG       := "firmware_path=/system/etc/wifi/bcm4330_apsta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
-WIFI_BAND                       := 802_11_ABG
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/dhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA := "/system/etc/wifi/bcm4330_sta.bin"
+WIFI_DRIVER_FW_PATH_AP := "/system/etc/wifi/bcm4330_apsta.bin"
+WIFI_DRIVER_FW_PATH_P2P := "/system/etc/wifi/bcm4330_p2p.bin"
+WIFI_DRIVER_MODULE_NAME := "dhd"
+WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/etc/wifi/bcm4330_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
+WIFI_DRIVER_MODULE_AP_ARG := "firmware_path=/system/etc/wifi/bcm4330_apsta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/dhd.ko"
+WIFI_BAND := 802_11_ABG
 BOARD_LEGACY_NL80211_STA_EVENTS := true
-BOARD_HAVE_SAMSUNG_WIFI         := true
+BOARD_HAVE_SAMSUNG_WIFI := true
 
 # SkTextBox for libtvout
 BOARD_USES_SKTEXTBOX := true
