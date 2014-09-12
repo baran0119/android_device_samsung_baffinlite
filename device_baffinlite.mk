bfdf585da0054ee186d52154312ddf4ebaf4a212
Artur <baran0119@users.noreply.github.com>
2014-09-12 14:37:54 +0200

    Update device_baffinlite.mk

--- device_baffinlite.mk
+++ device_baffinlite.mk
@@ -35,10 +35,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
 	audio.r_submix.default
 
-# OMX
-PRODUCT_PACKAGES += \
-	libdashplayer
-
 # Filesystem management tools
 PRODUCT_PACKAGES += \
 	setup_fs
